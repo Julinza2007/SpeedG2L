22818c47cae4cd50bbc6c0a795002dfa5f447b80
Julinza2007 <linzalatajulian@gmail.com>
2026-07-21 15:55:38 -0300

    Feat: agregamos la clase del juego SpeedG2L

--- core/src/main/java/com/g2l/speedg2l/SpeedG2L.java
+++ core/src/main/java/com/g2l/speedg2l/SpeedG2L.java
@@ -4,24 +4,28 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
-
+import com.g2l.speedg2l.entidades.Jugador;
 
 public class SpeedG2L extends Game {
 
     private SpriteBatch batch;
     private Texture image;
+    private Jugador jugador = new Jugador();
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+        jugador = new Jugador();
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        ScreenUtils.clear(0f, 0f, 0f, 0f);
+        jugador.moverJugador();
+        // y detectar las colisiones.
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(image, jugador.getPosicionX(), jugador.getPosicionY());
         batch.end();
     }
 
