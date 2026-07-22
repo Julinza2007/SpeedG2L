d037b51bbed0b4973323683190ebde52bd18719b
Julinza2007 <linzalatajulian@gmail.com>
2026-07-22 14:36:56 -0300

    Feat: Clase boton personalizada

---
+++ core/src/main/java/com/g2l/speedg2l/componentes/Boton.java
@@ -0,0 +1,11 @@
+package com.g2l.speedg2l.componentes;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
+public class Boton extends TextButton {
+
+    public Boton(String text, Skin skin) {
+        super(text, skin);
+    }
+}
