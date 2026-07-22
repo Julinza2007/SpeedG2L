cae7076ef7ca3566cd09b554ebfc7b3f905120ce
Julinza2007 <linzalatajulian@gmail.com>
2026-07-22 14:29:53 -0300

    Refactor: Implementamos entradas en el jugador

--- core/src/main/java/com/g2l/speedg2l/entidades/Jugador.java
+++ core/src/main/java/com/g2l/speedg2l/entidades/Jugador.java
@@ -1,41 +1,40 @@
 package com.g2l.speedg2l.entidades;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
+import com.g2l.speedg2l.utilidades.Entradas;
 
 public class Jugador extends Entidad{
-    private int velocidadX = 1;
-    private int velocidadY = 1;
+    private final int velocidadX = 1;
+    private final int velocidadY = 1;
     private int posicionX = 100;
     private int posicionY = 100;
 
-    public Jugador(){
+    public Jugador(float ancho , float alto){
         super(100, 100);
     }
 
-    public void moverJugador(){
-        if (Gdx.input.isKeyPressed(Input.Keys.A)){
+    public void moverJugador(Entradas entradas){
+        if (entradas.izquierda()){
             this.posicionX -= velocidadX;
         }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.D)){
+        if (entradas.derecha()){
             this.posicionX += velocidadX;
         }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.S)){
+        if (entradas.abajo()){
             this.posicionY -= velocidadY;
         }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.W)){
+        if (entradas.arriba()){
             this.posicionY += velocidadY;
         }
     }
 
-    public int getPosicionY(){
+    public float getPosicionY(){
         return this.posicionY;
     }
 
-    public int getPosicionX(){
+    public float getPosicionX(){
         return this.posicionX;
     }
 }
