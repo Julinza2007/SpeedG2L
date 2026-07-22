bc69dedc71fbfb15be9ec744f6ded5e53f276f55
Julinza2007 <linzalatajulian@gmail.com>
2026-07-22 14:36:00 -0300

    Feat: nuevo movimiento del jugador, implementacion: Render, PantallaMenu

--- core/src/main/java/com/g2l/speedg2l/SpeedG2L.java
+++ core/src/main/java/com/g2l/speedg2l/SpeedG2L.java
@@ -1,37 +1,44 @@
 package com.g2l.speedg2l;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.g2l.speedg2l.componentes.Imagen;
 import com.g2l.speedg2l.entidades.Jugador;
+import com.g2l.speedg2l.pantallas.PantallaMenu;
+import com.g2l.speedg2l.utilidades.Entradas;
+import com.g2l.speedg2l.utilidades.Render;
 
 public class SpeedG2L extends Game {
 
-    private SpriteBatch batch;
-    private Texture image;
-    private Jugador jugador = new Jugador();
+    private SpriteBatch b;
+    private Imagen logo;
+    private Jugador jugador;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-        jugador = new Jugador();
+        this.setScreen(new PantallaMenu());
+        b = Render.batch;
+
+        jugador = new Jugador(100.0f, 100.0f);
+        logo = new Imagen("libgdx.png");
+        logo.setSize(jugador.getAncho(), jugador.getAlto());
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0f, 0f, 0f, 0f);
-        jugador.moverJugador();
-        // y detectar las colisiones.
-        batch.begin();
-        batch.draw(image, jugador.getPosicionX(), jugador.getPosicionY());
-        batch.end();
+        Render.limpiarPantalla();
+        super.render();
+
+        jugador.moverJugador(new Entradas());
+        b.begin();
+        logo.setX(jugador.getPosicionX());
+        logo.setY(jugador.getPosicionY());
+        logo.dibujar();
+        b.end();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        b.dispose();
     }
 }
