af11a9f7921f286ca2237467a58b4401c3b33045
Julinza2007 <linzalatajulian@gmail.com>
2026-07-23 13:18:31 -0300

    Feat: Agregado de utilidad para configuracion general

---
+++ core/src/main/java/com/g2l/speedg2l/utilidades/Config.java
@@ -0,0 +1,15 @@
+package com.g2l.speedg2l.utilidades;
+
+public class Config {
+    public static int ancho = 1280;
+    public static int alto = 720;
+    private Config(){}
+
+    public static int getAncho() {
+        return ancho;
+    }
+
+    public static int getAlto() {
+        return alto;
+    }
+}
