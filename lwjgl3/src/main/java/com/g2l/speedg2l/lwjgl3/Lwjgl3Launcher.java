ce77075658e1ebcaca2fc6c3aa1518052b5b8ae9
Julinza2007 <linzalatajulian@gmail.com>
2026-08-24 23:39:00 -0300

    Feat: Agregado de pantalla completa al iniciar el juego.

--- lwjgl3/src/main/java/com/g2l/speedg2l/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/g2l/speedg2l/lwjgl3/Lwjgl3Launcher.java
@@ -1,28 +1,30 @@
 package com.g2l.speedg2l.lwjgl3;
 
+import com.badlogic.gdx.Graphics.DisplayMode;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.g2l.speedg2l.SpeedG2L;
-import com.g2l.speedg2l.utilidades.Config;
 
-/** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
+
+    public static final String TITULO = "SpeedG2L";
+
     public static void main(String[] args) {
-        if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
+        if (StartupHelper.startNewJvmIfRequired()) return;
         createApplication();
     }
 
-    public static final String TITULO = "SpeedG2L";
-    public static int V_WIDTH = Config.ancho;
-    public static int V_HEIGHT = Config.alto;
-    public static final int SCALE = 1;
-
     private static void createApplication() {
-        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        Lwjgl3ApplicationConfiguration config =
+            new Lwjgl3ApplicationConfiguration();
+
+        DisplayMode modoMonitor =
+            Lwjgl3ApplicationConfiguration.getDisplayMode();
 
         config.setTitle(TITULO);
-        config.setWindowedMode((V_WIDTH * SCALE),(V_HEIGHT * SCALE));
 
-       new Lwjgl3Application(new SpeedG2L(), config);
+        config.setFullscreenMode(modoMonitor);
+
+        new Lwjgl3Application(new SpeedG2L(), config);
     }
 }
