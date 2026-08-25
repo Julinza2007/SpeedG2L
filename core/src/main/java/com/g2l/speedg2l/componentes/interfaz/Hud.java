cf0db30836c65f81c9833109a40a5a2c98bef24d
eluney <eluneylaje@gmail.com>
2026-08-25 09:52:56 -0300

    Feat: Se agrego la clase Hud, no tiene contenido funcional

---
+++ core/src/main/java/com/g2l/speedg2l/componentes/interfaz/Hud.java
@@ -0,0 +1,22 @@
+package com.g2l.speedg2l.componentes.interfaz;
+
+import com.g2l.speedg2l.componentes.Texto;
+
+public class Hud {
+
+    private Texto cronometro;
+    private Texto posicion;
+
+    public Hud() {
+    }
+
+    public void actualizar() {
+    }
+
+    public void dibujar() {
+    }
+
+    public void dispose() {
+    }
+
+}
