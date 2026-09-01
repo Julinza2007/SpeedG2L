edefa99d6fea524536fa19ef6957ead40db95af2
gimeneznacho07 <136719918+gimeneznacho07@users.noreply.github.com>
2026-09-01 00:05:01 -0300

    FIX: Arreglo el contenido del Tileset

--- assets/nivel/Tileset (2).tsx
+++ assets/nivel/Tileset (2).tsx
@@ -1,34 +1,29 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.2" name="Conjunto" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../../Tiles Set.png" width="512" height="512"/>
- <tile id="0" type="Piso">
-  <properties>
-   <property name="solido = true" type="bool" value="true"/>
-  </properties>
- </tile>
- <tile id="2" type="Techo">
-  <properties>
-   <property name="solido = true" type="bool" value="true"/>
-  </properties>
- </tile>
- <tile id="4" type="Plataforma 1">
-  <properties>
-   <property name="solido = true" type="bool" value="true"/>
-  </properties>
- </tile>
- <tile id="5" type="Plataforma 1.2">
-  <properties>
-   <property name="solido = true" type="bool" value="true"/>
-  </properties>
- </tile>
- <tile id="7" type="Plataforma 2">
-  <properties>
-   <property name="solido = true" type="bool" value="true"/>
-  </properties>
- </tile>
- <tile id="9" type="Lava">
-  <properties>
-   <property name="mortal = true" type="bool" value="true"/>
-  </properties>
- </tile>
+    <image source="Tiles Set.png" width="512" height="512"/>
+    <tile id="0" type="Piso">
+    <properties>
+    <property name="solido" type="bool" value="true"/>
+    </properties>
+</tile>
+<tile id="2" type="Techo">
+    <properties>
+        <property name="solido" type="bool" value="true"/>
+    </properties>
+</tile>
+<tile id="4" type="Plataforma 1">
+    <properties>
+        <property name="solido" type="bool" value="true"/>
+    </properties>
+</tile>
+<tile id="5" type="Plataforma 1.2">
+    <properties>
+        <property name="solido" type="bool" value="true"/>
+    </properties>
+</tile>
+<tile id="7" type="Plataforma 2">
+    <properties>
+        <property name="solido" type="bool" value="true"/>
+    </properties>
+</tile>
 </tileset>
